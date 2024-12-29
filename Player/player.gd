88d0df2e02c462fa60c77a3b0d427cfd3e86845f
dcerniauskas <125649197+dcerniauskas@users.noreply.github.com>
2024-12-29 17:23:33 +0200

    Lecture 69 Input events and Aiming
    Lecture 69 Input events and Aiming
    added mouse look horizontally

--- Player/player.gd
+++ Player/player.gd
@@ -4,12 +4,18 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+var mouse_motion := Vector2.ZERO
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta: float) -> void:
+	handle_camera_rotation()
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -26,3 +32,18 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+			mouse_motion = -event.relative * 0.01
+	if event.is_action_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		
+func handle_camera_rotation() -> void:
+	if Input.MOUSE_MODE_CAPTURED:
+		rotate_y(mouse_motion.x)
+		mouse_motion = Vector2.ZERO
+	
+		#rotate_x(mouse_motion.y)
+		#mouse_motion = Vector2.ZERO
